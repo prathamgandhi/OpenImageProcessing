6d4e258d416c6eb05fc33746030519c3c0ac837b
Pratham Gandhi <ppg.1382@gmail.com>
2023-03-26 00:07:32 +0530

    Modified gitignore and modified build scripts

--- app/src/main/java/com/example/openimageprocessing/MainActivity.java
+++ app/src/main/java/com/example/openimageprocessing/MainActivity.java
@@ -52,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         pickImage = findViewById(R.id.pickImage);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
