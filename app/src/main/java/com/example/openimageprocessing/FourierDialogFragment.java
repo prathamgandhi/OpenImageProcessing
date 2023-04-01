61d9f5d9a11ddb720e4dfb266dfb77150e441e78
Pratham Gandhi <ppg.1382@gmail.com>
2023-04-01 23:51:13 +0530

    added the code for creating magnitude and phase images from dft

--- app/src/main/java/com/example/openimageprocessing/FourierDialogFragment.java
+++ app/src/main/java/com/example/openimageprocessing/FourierDialogFragment.java
@@ -33,6 +33,7 @@ public class FourierDialogFragment extends DialogFragment {
 
     private static final String TAG = "FourierDialogFrag";
 
+    ImageView imageEditorView;
 
 
     public interface FourierListener{
@@ -40,16 +41,6 @@ public class FourierDialogFragment extends DialogFragment {
     }
 
     public FourierListener fourierListener;
-
-
-    // @Override
-    // public void onResume() {
-    //     super.onResume();
-
-    //     // Without these 2 lines the soft keyboard won't popup.
-    //     getDialog().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
-    //     getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-    // }
 
     @NonNull
     @Override
@@ -63,6 +54,87 @@ public class FourierDialogFragment extends DialogFragment {
 
         Dialog dialog = builder.create();
 
+        ImageView imageEditorView = getActivity().findViewById(R.id.imageEditorView);
+        src = new Mat();
+        Utils.bitmapToMat(imageLoader, src);
+        src.convertTo(src, CvType.CV_8UC1);
+
+        Mat padded = new Mat();                     //expand input image to optimal size
+        int m = Core.getOptimalDFTSize( I.rows() );
+        int n = Core.getOptimalDFTSize( I.cols() ); // on the border add zero values
+        Core.copyMakeBorder(src, padded, 0, m - src.rows(), 0, n - src.cols(), Core.BORDER_CONSTANT, Scalar.all(0));
+
+        List<Mat> planes = new ArrayList<Mat>();
+        padded.convertTo(padded, CvType.CV_32F);
+        planes.add(padded);
+        planes.add(Mat.zeros(padded.size(), CvType.CV_32F));
+        Mat complexI = new Mat();
+        Core.merge(planes, complexI);         // Add to the expanded another plane with zeros
+        Core.dft(complexI, complexI);         // this way the result may fit in the source matrix
+
+        Core.split(complexI, planes);                               // planes.get(0) = Re(DFT(I)
+                                                                    // planes.get(1) = Im(DFT(I))
+
+        Mat magI = new Mat(src.rows(), src.cols(), src.type());
+        Mat phaseI = new Mat(src.rows(), src.cols(), src.type());
+        Core.magnitude(planes.get(0), planes.get(1), magI);// planes.get(0) = magnitude
+        Core.phase(planes.get(0), planes.get(1), phaseI);
+
+        Mat matOfOnesMag = Mat.ones(magI.size(), magI.type());
+        Core.add(matOfOnesMag, magI, magI);         // switch to logarithmic scale
+        Core.log(magI, magI);
+
+        Mat matOfOnesPhase = Mat.ones(phaseI.size(), phaseI.type());
+        Core.add(matOfOnes, phaseI, phaseI);         // switch to logarithmic scale
+        Core.log(phaseI, phaseI);
+
+        // For magnitude
+
+        magI = magI.submat(new Rect(0, 0, magI.cols() & -2, magI.rows() & -2));
+        // rearrange the quadrants of Fourier image  so that the origin is at the image center
+        int cx = magI.cols()/2;
+        int cy = magI.rows()/2;
+        Mat q0 = new Mat(magI, new Rect(0, 0, cx, cy));   // Top-Left - Create a ROI per quadrant
+        Mat q1 = new Mat(magI, new Rect(cx, 0, cx, cy));  // Top-Right
+        Mat q2 = new Mat(magI, new Rect(0, cy, cx, cy));  // Bottom-Left
+        Mat q3 = new Mat(magI, new Rect(cx, cy, cx, cy)); // Bottom-Right
+        Mat tmp = new Mat();               // swap quadrants (Top-Left with Bottom-Right)
+        q0.copyTo(tmp);
+        q3.copyTo(q0);
+        tmp.copyTo(q3);
+        q1.copyTo(tmp);                    // swap quadrant (Top-Right with Bottom-Left)
+        q2.copyTo(q1);
+        tmp.copyTo(q2);
+        magI.convertTo(magI, CvType.CV_8UC1);
+        Core.normalize(magI, magI, 0, 255, Core.NORM_MINMAX, CvType.CV_8UC1);
+
+        // For phase
+
+        phaseI = phaseI.submat(new Rect(0, 0, phaseI.cols() & -2, phaseI.rows() & -2));
+        // rearrange the quadrants of Fourier image  so that the origin is at the image center
+        int cx = phaseI.cols()/2;
+        int cy = phaseI.rows()/2;
+        Mat q0 = new Mat(phaseI, new Rect(0, 0, cx, cy));   // Top-Left - Create a ROI per quadrant
+        Mat q1 = new Mat(phaseI, new Rect(cx, 0, cx, cy));  // Top-Right
+        Mat q2 = new Mat(phaseI, new Rect(0, cy, cx, cy));  // Bottom-Left
+        Mat q3 = new Mat(phaseI, new Rect(cx, cy, cx, cy)); // Bottom-Right
+        Mat tmp = new Mat();               // swap quadrants (Top-Left with Bottom-Right)
+        q0.copyTo(tmp);
+        q3.copyTo(q0);
+        tmp.copyTo(q3);
+        q1.copyTo(tmp);                    // swap quadrant (Top-Right with Bottom-Left)
+        q2.copyTo(q1);
+        tmp.copyTo(q2);
+        phaseI.convertTo(phaseI, CvType.CV_8UC1);
+        Core.normalize(phaseI, phaseI, 0, 255, Core.NORM_MINMAX, CvType.CV_8UC1);
+
+        Bitmap magnitudeLoader, phaseLoader;
+        Utils.matToBitmap(magI, magnitudeLoader);
+        Utils.matToBitmap(phaseI, phaseLoader);
+        ImageView magnitudeLoaderView  = fourierTransformDialogView.findViewById(R.id.Amplitude);
+        ImageView phaseLoaderView = fourierTransformDialogView.findViewById(R.id.phase);
+        magnitudeLoaderView.setImageBitmap(magnitudeLoader);
+        phaseLoaderView.setImageBitmap(phaseLoader);
 
         dialog.setOnShowListener(dialogInterface -> {
             
